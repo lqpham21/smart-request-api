c2ac81a1913d002bcd35ccbc99a3cca5c2edfabe
lqpham21 <lqpham21@yahoo.com>
2025-07-21 11:16:10 -0500

    Database Connection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using SmartRequest.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
